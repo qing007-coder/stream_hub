b1e6cdb5c75dba2ac46928f3fbb082ab5bb8c1f8
qing <117420744+qing007-coder@users.noreply.github.com>
2026-02-13 17:49:24 +0800

    feat: add the task internal function about tranform from map to struction

--- pkg/model/infra/task.go
+++ pkg/model/infra/task.go
@@ -1,6 +1,10 @@
 package infra
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+)
 
 type TaskMessage struct {
 	TaskID     string      `json:"task_id"`
@@ -16,4 +20,22 @@ type TaskPayload struct {
 	Source   string          `json:"source"`
 	Action   string          `json:"action"`
 	Data     json.RawMessage `json:"data"`
+}
+
+func (t *TaskMessage) TransformByMap(data map[string]string) error {
+	if data["task_id"] == "" {
+		return errors.New("missing task_id")
+	}
+
+	t.TaskID = data["task_id"]
+	t.Type = data["type"]
+	t.BizID = data["biz_id"]
+	t.Priority = data["priority"]
+	retryCount, err := strconv.Atoi(data["retry_count"])
+	if err != nil {
+		return errors.New("invalid retry_count: " + err.Error())
+	}
+	t.RetryCount = retryCount
+
+	return nil
 }
