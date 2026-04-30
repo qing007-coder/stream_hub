f0282818ae1e8480ff96dbaadb34e048e8d73df1
qing <117420744+qing007-coder@users.noreply.github.com>
2026-04-30 21:01:47 +0800

    fix: 添加任务

--- cmd/components/scheduler/main.go
+++ cmd/components/scheduler/main.go
@@ -7,6 +7,8 @@ import (
 	"stream_hub/internal/infra"
 	"stream_hub/pkg/config"
 	"stream_hub/pkg/constant"
+	infra_ "stream_hub/pkg/model/infra"
+	"time"
 )
 
 func main() {
@@ -28,13 +30,38 @@ func main() {
 		return
 	}
 
-	handler := task_handler.NewCommonTaskHandler(commonConf, base)
+	handler, err := task_handler.NewTaskHandler(commonConf, base)
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
 
 	server := core.NewServer(base.DB, base.Redis, schedulerConf)
 
 	serveMux := core.NewServeMux()
 	serveMux.HandleFunc(constant.TaskSendEmailCode, handler.EmailHandler)
 	serveMux.HandleFunc(constant.TaskVideoTranscode, handler.TranscodeHandler)
+	serveMux.HandleFunc(constant.TaskCalculateFeed, handler.CalculateFeed)
+	serveMux.HandleFunc(constant.TaskVideoAudit, handler.AuditVideo)
+
+	taskSender, err := infra.NewTaskSender(commonConf)
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+
+	// 初始化定时任务种子
+	taskSender.SendDelayTask(infra_.TaskMessage{
+		Type:     constant.TaskCalculateFeed,
+		BizID:    "",
+		Priority: "critical",
+		Payload: infra_.TaskPayload{
+			Operator: "",
+			Action:   "",
+			Source:   constant.Scheduler,
+			Data:     nil,
+		},
+	}, time.Hour*2)
 
 	server.RegisterServeMux(serveMux)
 
